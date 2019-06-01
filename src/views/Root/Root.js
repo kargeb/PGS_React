9f38c49e60867b2fba31759e4811723bbbbdab60
Karol Gebarowski <karol.gebarowski@gmail.com>
2019-06-01 11:44:05 +0200

    First commit

---
+++ src/views/Root/Root.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Root.module.css";
+
+const Root = () => {
+  return (
+    <div>
+      <h1 className={styles.mainHeader}>Root jest </h1>
+    </div>
+  );
+};
+
+export default Root;
